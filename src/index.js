560c8fa0dfdc1ab93a71fa85625f48daeb6e95d5
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-01-10 19:52:38 +0300

    feat: add new solution

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
 module.exports = function reverse (n) {
-    let digit, result = 0
-
-    while( n ){
-        digit = n % 10  
-        result = (result * 10) + digit  
-        n = n/10|0  
-    }  
-  
-    return Math.abs(result);
+    n = Math.abs(n);
+    return +String(n).split("").reverse().join("");
 }
